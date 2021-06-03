a1d90fe6936c9ef955d49bc13ba88eee8cf726f9
吴wbh1328551759 <1328551759@qq.com>
2021-06-03 15:35:56 +0800

    feat: update PCX type and  finish feature of transfer

--- src/components/Content/TokenSelect/index.tsx
+++ src/components/Content/TokenSelect/index.tsx
@@ -1,25 +1,27 @@
 import React from 'react';
 import { Select } from 'antd';
-const { Option } = Select;
+
+const {Option} = Select;
 
 function TokenSelect(): React.ReactElement {
-    const coinList = ['PCX','KSX','XBTC'];
-    function handleChange(value: any) {
-        console.log(`Selected: ${value}`);
-    }
-    return (
-        <div className='flex flex-col items-center justify-center w-full'>
-            <Select 
-                placeholder="Select a token"
-                // defaultValue="PCX" 
-                onChange={handleChange} 
-            >
-                {coinList.map((item)=>{
-                    return <Option key={item} value={item}>{item}</Option>
-                })}
-            </Select>
-        </div>
-    );
+  const coinList = ['PCX', 'KSX', 'XBTC'];
+
+  function handleChange(value: any) {
+    console.log(`Selected: ${value}`);
+  }
+
+  return (
+    <div className='flex flex-col items-center justify-center w-full'>
+      <Select
+        placeholder="Select a token"
+        onChange={handleChange}
+      >
+        {coinList.map((item) => {
+          return <Option key={item} value={item}>{item}</Option>;
+        })}
+      </Select>
+    </div>
+  );
 }
 
 export default TokenSelect;
